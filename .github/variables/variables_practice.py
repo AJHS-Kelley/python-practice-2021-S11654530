455bbc4e0aeb8b625fb01280cd8f3cd4abd87065
Ronald Durham <70582212+S11654530@users.noreply.github.com>
2021-10-29 14:51:40 -0400

    Update variables_practice.py

---
+++ .github/variables/variables_practice.py
@@ -0,0 +1,12 @@
+# Variables practice, Ronald Durham, 10/29/21 2:45 pm, version 0.0
+
+# Three common types of data 
+
+lives_remaining = 13 # INTERGERS , any +/- whole number and 0.
+forty_time = 4.21 # FLOATING POINT , any +/- number with a deciml.
+death_cry = "i cant stand you"
+# line 7 is a STRING -- collection of letters/numbers/other chars 
+
+print(lives_remaining)
+print(forty_time)
+print(death_cry)
